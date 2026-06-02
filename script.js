0591b5045829f6d15bd58147e9159bc969b4755c
philz-codez <memetic.trivia1t@icloud.com>
2026-06-02 08:34:50 -0400

    fixing API key error

--- script.js
+++ script.js
@@ -155,7 +155,7 @@ function typeMessage(text, sender) {
 }
 
 async function fetchGeminiReply(promptText, signal) {
-    const url = `https://generativelanguage.googleapis.com/v1/models/gemini-2.5-flash:generateContent?key=${API_KEY}`;
+    const url = `https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent?key=${API_KEY}`;
 
     
     const requestBody = {
